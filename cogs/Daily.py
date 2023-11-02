85d528f42684f4a7db68afa7989db46e4b4897f7
Dylan Vidal <dylanvidal1204@gmail.com>
2023-11-02 16:13:36 -0400

    Update for Heroku Implementation

--- cogs/Daily.py
+++ cogs/Daily.py
@@ -6,6 +6,7 @@ import nextcord
 from nextcord.ext import commands
 import asyncio
 from datetime import datetime, timedelta
+import os
 import requests, bs4
 import re
 
@@ -67,8 +68,12 @@ def is_today_or_tomorrow(date_str):
 
 def scrape_events():
  
-    # Open a new Chrome window and scrape
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get("https://knightconnect.campuslabs.com/engage/events")
     wait = WebDriverWait(driver, 20)  
 
